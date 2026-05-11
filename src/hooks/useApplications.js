ae2dbb3fe417f807ad5d03814d00d7714025f959
Simbongile Mkhotheli <mkotelisimbo@gmail.com>
2026-05-11 15:51:56 +0200

    feat: persist application state with local storage

--- src/hooks/useApplications.js
+++ src/hooks/useApplications.js
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { mockApplications } from "../data/mockApplications";
+import { applicationService } from "../services/applicationService";
 
 export function useApplications() {
-  const [applications, setApplications] = useState(mockApplications);
+  const [applications, setApplications] = useState(() => {
+    const storedApplications =
+      applicationService.getApplications();
+
+    return storedApplications.length > 0
+      ? storedApplications
+      : mockApplications;
+  });
+
+  useEffect(() => {
+    applicationService.saveApplications(applications);
+  }, [applications]);
 
   function addApplication(application) {
     const newApplication = {
@@ -18,7 +30,9 @@ export function useApplications() {
 
   function deleteApplication(id) {
     setApplications((currentApplications) =>
-      currentApplications.filter((application) => application.id !== id)
+      currentApplications.filter(
+        (application) => application.id !== id
+      )
     );
   }
 
